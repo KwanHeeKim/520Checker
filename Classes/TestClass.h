bc1d714cc74fe752aac1e8c50c45c61b93e1b8b3
KwanHeeKim <ohkaning@gmail.com>
2011-08-19 13:41:58 +0900

    test1
    test1

---
+++ Classes/TestClass.h
@@ -0,0 +1,16 @@
+//
+//  TestClass.h
+//  Pension520Checker
+//
+//  Created by KwanHee Kim on 11. 8. 19..
+//  Copyright 2011 Nubicom. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface TestClass : NSObject {
+
+}
+
+@end
---
+++ Classes/TestClass.m
@@ -0,0 +1,14 @@
+//
+//  TestClass.m
+//  Pension520Checker
+//
+//  Created by KwanHee Kim on 11. 8. 19..
+//  Copyright 2011 Nubicom. All rights reserved.
+//
+
+#import "TestClass.h"
+
+
+@implementation TestClass
+
+@end
